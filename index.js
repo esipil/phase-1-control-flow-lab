62d98c0be80c925e69deca6f2763da5aaf627b90
Timothy <lemayiant4@gmail.com>
2022-08-16 11:49:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400) {
+    result = 'This one is on me!' ;
+  }
+  else if ( feet > 2000) {
+    result= 'I will gladly take your thirty bucks.' ;
+  }
+  if (feet > 2500 ) {
+    result = 'No can do.'  ;
+  }
+  return result ;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   const place = 'NYC'
+   const whereTo =  city === place? 'Ok, sounds good.' : 'No go.' ;
+   return whereTo ;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip ){
+  
+  let gratitude ;
+
+   switch( tip ) {
+    case "generous" :
+      gratitude = "Thank you so much." ;
+      break ;
+    case "not as generous" :
+      gratitude = "Thank you." ;
+      break;
+    default:
+      gratitude = "Bye."
+  }
+  return gratitude;
 }
